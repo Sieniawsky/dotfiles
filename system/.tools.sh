bd4f0d361e47fefc4e6772ca87e8f694a20b2827
Martin Sieniawski <m.sieniawski@live.ca>
2021-01-04 11:40:22 -0500

    Bash completion

--- system/.tools.sh
+++ system/.tools.sh
@@ -3,3 +3,6 @@ source $HOME/.rvm/scripts/rvm
 
 # SCM Breeze
 [ -s "$HOME/.scm_breeze/scm_breeze.sh" ] && source "$HOME/.scm_breeze/scm_breeze.sh"
+
+# Bash completion
+[[ -r "/usr/local/etc/profile.d/bash_completion.sh" ]] && . "/usr/local/etc/profile.d/bash_completion.sh"
